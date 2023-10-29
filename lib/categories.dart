a3b2b1c5610d1b6819a212be343a172b6554be97
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2023-10-29 16:59:02 +0100

    fix problems

--- lib/categories.dart
+++ lib/categories.dart
@@ -29,12 +29,12 @@ Future<List<Categories>> fetchCategories() async {
 //DELETE
 
 Future<http.Response> deleteCategory(String cgid) async {
-  final http.Response response = await http.delete(Uri.parse('https://localhost:44393/api/Categories/Delete/$cgid'));
+  final http.Response response = await http
+      .delete(Uri.parse('https://localhost:44393/api/Categories/Delete/$cgid'));
 
-  if(response.statusCode == 200){
+  if (response.statusCode == 200) {
     fetchCategories();
-  }
-  else {
+  } else {
     Exception('Usunięcie kategorii zakończyło się niepowodzeniem');
   }
   return response;
@@ -88,93 +88,116 @@ class _CategoriesPageState extends State<CategoriesPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      children: [
-        if(isEditEnabled)
-          EditDataWidget(),
-        Row(children: const <Widget>[
-          Expanded(child: Text("Nazwa kategorii")),
-          Expanded(child: Text("Data początku")),
-          Expanded(child: Text("Data końca")),
-          Expanded(child: Text("Budżet")),
-        ]),
-        Center(
-          child: FutureBuilder<List<Categories>>(
-            future: futurePhotos,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: 100,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 8.0,
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 6.0),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(5)),
-                          color: Color.fromRGBO(225, 225, 225, 1),
-                        ),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 10.0),
-                          subtitle: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    snapshot.data![index].cName,
-                                    style: const TextStyle(
-                                      color: Colors.black,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const Text(
+                "Kategorie",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isEditEnabled = true;
+                  });
+                },
+                child: const Text('Dodaj'),
+              ),
+            ],
+          ),
+          if (isEditEnabled) EditDataWidget(),
+          Row(
+            children: const <Widget>[
+              Expanded(child: Text("Nazwa kategorii")),
+              Expanded(child: Text("Data początku")),
+              Expanded(child: Text("Data końca")),
+              Expanded(child: Text("Budżet")),
+            ],
+          ),
+          Center(
+            child: FutureBuilder<List<Categories>>(
+              future: futurePhotos,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 8.0,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 6.0),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            color: Color.fromRGBO(225, 225, 225, 1),
+                          ),
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            subtitle: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      snapshot.data![index].cName,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    snapshot.data![index].cStartDate,
-                                    style: const TextStyle(
-                                      color: Colors.black,
+                                  Expanded(
+                                    child: Text(
+                                      snapshot.data![index].cStartDate,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    snapshot.data![index].cEndDate,
-                                    style: const TextStyle(
-                                      color: Colors.black,
+                                  Expanded(
+                                    child: Text(
+                                      snapshot.data![index].cEndDate,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    snapshot.data![index].cBudget.toString(),
-                                    style: const TextStyle(
-                                      color: Colors.black,
+                                  Expanded(
+                                    child: Text(
+                                      snapshot.data![index].cBudget.toString(),
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                EditButton(cgid: snapshot.data![index].cgid),
-                                DeleteButton(cgid: snapshot.data![index].cgid),
-                              ],
+                                  EditButton(cgid: snapshot.data![index].cgid),
+                                  DeleteButton(
+                                      cgid: snapshot.data![index].cgid),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
 
-              return const CircularProgressIndicator();
-            },
+                return const CircularProgressIndicator();
+              },
+            ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
 
@@ -233,10 +256,10 @@ class EditDataWidget extends StatefulWidget {
 }
 
 class _EditDataWidgetState extends State<EditDataWidget> {
-  String name = '';
-  DateTime startDate = DateTime.now();
-  DateTime endDate = DateTime.now();
-  double budget = 0.0;
+  String cName = '';
+  DateTime cStartDate = DateTime.now();
+  DateTime cEndDate = DateTime.now();
+  double cBudget = 0.0;
 
   // void _showStartDatePicker(BuildContext context) async {
   //   DateTime pickedDate = await showDatePicker(
@@ -273,57 +296,80 @@ class _EditDataWidgetState extends State<EditDataWidget> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Nazwa kategorii'),
-              onChanged: (value) {
-                setState(() {
-                  name = value;
-                });
-              },
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Nazwa kategorii'),
+                onChanged: (value) {
+                  setState(() {
+                    cName = value;
+                  });
+                },
+              ),
             ),
             ListTile(
               title: const Text('Data początku'),
-              subtitle: Text('${startDate.toLocal()}'.split(' ')[0]),
+              subtitle: Text('${cStartDate.toLocal()}'.split(' ')[0]),
               onTap: () {
                 //_showStartDatePicker(context);
               },
             ),
             ListTile(
               title: const Text('Data końca'),
-              subtitle: Text('${endDate.toLocal()}'.split(' ')[0]),
+              subtitle: Text('${cEndDate.toLocal()}'.split(' ')[0]),
               onTap: () {
                 //_showEndDatePicker(context);
               },
             ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Budżet'),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  budget = double.tryParse(value) ?? 0.0;
-                });
-              },
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Budżet'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    cBudget = double.tryParse(value) ?? 0.0;
+                  });
+                },
+              ),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Handle saving the data here
-                print('Name: $name');
-                print('Start Date: $startDate');
-                print('End Date: $endDate');
-                print('Budget: $budget');
-              },
-              child: const Text('Zapisz'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Handle saving the data here
-                print('Name: $name');
-                print('Start Date: $startDate');
-                print('End Date: $endDate');
-                print('Budget: $budget');
-              },
-              child: const Text('Anuluj'),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle saving the data here
+                      print('Name: $cName');
+                      print('Start Date: $cStartDate');
+                      print('End Date: $cEndDate');
+                      print('Budget: $cBudget');
+                    },
+                    child: const Text('Zapisz'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        cName = '';
+                        cStartDate = DateTime.now();
+                        cEndDate = DateTime.now();
+                        cBudget = 0.0;
+                      });
+                      var state = context
+                          .findAncestorStateOfType<_CategoriesPageState>();
+                      state?.setState(() {
+                        state.isEditEnabled = false;
+                      });
+                    },
+                    child: const Text('Anuluj'),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
