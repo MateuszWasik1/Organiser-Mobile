45d9c64a2241e1c5aeeab59d3a83b551b12651ee
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2023-11-25 12:38:53 +0100

    Edit method - first attempt

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -52,6 +52,20 @@ Future<List<Categories>> fetchCategories() async {
   }
 }
 
+//Add / Edit
+
+Future<http.Response> editTask(Tasks task) async {
+  final http.Response response = await http.post(
+      Uri.parse('https://localhost:44393/api/Tasks/Save'),
+      body: task);
+
+  if (response.statusCode == 200) {
+  } else {
+    Exception('Dodanie taska zakończyło się niepowodzeniem');
+  }
+  return response;
+}
+
 //DELETE
 Future<http.Response> deleteTask(String tgid) async {
   final http.Response response = await http
@@ -143,13 +157,24 @@ class _TasksPageState extends State<TasksPage> {
   late Future<List<Tasks>> futureTasks;
   late Future<List<Categories>> futureCategories;
   late bool isEditEnabled = false;
-  late Categories category = Categories(
-      cid: 0,
-      cgid: "",
-      cName: "",
-      cStartDate: DateTime.now().toString(),
-      cEndDate: DateTime.now().toString(),
-      cBudget: 0);
+  // late Categories category = Categories(
+  //     cid: 0,
+  //     cgid: "",
+  //     cName: "",
+  //     cStartDate: DateTime.now().toString(),
+  //     cEndDate: DateTime.now().toString(),
+  //     cBudget: 0);
+  late Tasks task = Tasks(
+    tid: 0, 
+    tgid: "", 
+    tuid: 0, 
+    tcgid: "", 
+    tName: "", 
+    tLocalization: "", 
+    tTime: DateTime.now().toString(), 
+    tBudget: 0, 
+    tStatus: 0
+  );
 
   @override
   void initState() {
@@ -188,7 +213,7 @@ class _TasksPageState extends State<TasksPage> {
               ],
             ),
           ),
-          //if (isEditEnabled) EditDataWidget(category: category),
+          if (isEditEnabled) EditDataWidget(task: task),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Row(
@@ -301,6 +326,32 @@ class _TasksPageState extends State<TasksPage> {
 }
 
 //Buttons
+class EditButton extends StatelessWidget {
+  const EditButton({super.key, required this.task});
+
+  final Tasks task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.edit),
+          tooltip: 'Edytuj taska',
+          onPressed: () {
+            var tasksState =
+                context.findAncestorStateOfType<_TasksPageState>();
+            tasksState?.setState(() {
+              tasksState.isEditEnabled = true;
+              tasksState.task = task;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
 
 class DeleteButton extends StatelessWidget {
   const DeleteButton({super.key, required this.tgid});
@@ -317,6 +368,128 @@ class DeleteButton extends StatelessWidget {
             tooltip: 'Usuń Task',
             onPressed: () => deleteTask(tgid)),
       ],
+    );
+  }
+}
+
+//EditWidget
+// ignore: must_be_immutable
+class EditDataWidget extends StatefulWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  var task;
+
+  EditDataWidget({Key? key, this.task}) : super(key: key);
+
+  @override
+  State<EditDataWidget> createState() => _EditDataWidgetState();
+}
+
+class _EditDataWidgetState extends State<EditDataWidget> {
+  var tName = "";
+  var tTime = DateTime.now();
+  var tLocalization = "";
+  var tBudget = 0;
+
+  void _datePicker(BuildContext context) async {
+    DateTime pickedDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.parse(widget.task.tTime),
+          firstDate: DateTime(2000),
+          lastDate: DateTime(2101),
+        ) ??
+        DateTime.now();
+
+    tTime = pickedDate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: TextFormField(
+                initialValue: widget.task.tName,
+                decoration: const InputDecoration(labelText: 'Nazwa'),
+                onChanged: (value) => tName = value,
+              ),
+            ),
+            ListTile(
+              title: const Text('Data'),
+              subtitle: Text(
+                '${DateTime.parse(widget.task.tTime).toLocal()}'
+                    .split(' ')[0],
+              ),
+              onTap: () {
+                _datePicker(context);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: TextFormField(
+                  initialValue: widget.task.tLocalization.toString(),
+                  decoration: const InputDecoration(labelText: 'Lokalizacja'),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    tLocalization = value;
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: TextFormField(
+                  initialValue: widget.task.tBudget.toString(),
+                  decoration: const InputDecoration(labelText: 'Budżet'),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    tBudget = int.tryParse(value) ?? 0;
+                  }),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      var taskToSave = Tasks(
+                          tid: widget.task.tid,
+                          tgid: widget.task.tgid,
+                          tuid: widget.task.tuid,
+                          tcgid: widget.task.tcgid,
+                          tName: tName,
+                          tLocalization: tLocalization,
+                          tTime: tTime.toString(),
+                          tBudget: tBudget,
+                          tStatus: 0,
+                        );
+
+                      editTask(taskToSave);
+                    },
+                    child: const Text('Zapisz'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      var state = context
+                          .findAncestorStateOfType<_TasksPageState>();
+                      state?.setState(() {
+                        state.isEditEnabled = false;
+                      });
+                    },
+                    child: const Text('Anuluj'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
